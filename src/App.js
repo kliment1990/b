86a4406df597f339853239d96c4dfafa2d1d7c57
Klim <kklimmentt@gmail.com>
2022-01-26 06:43:49 +0200

    kj

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Body from "./Components/Body/Body";
+
+function App() {
+  return (
+    <>
+      <Body />
+    </>
+  );
+}
+
+export default App;
